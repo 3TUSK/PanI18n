69bc0ad0d9d7e4675e8e91863b88d8f0197b711a
3TUSK <urey.s.knowledge@gmail.com>
2017-03-25 20:08:20 -0400

    Simplify code

--- src/main/java/info/tritusk/fullwidthpunctuationfix/CommandDumpGlyphWidth.java
+++ src/main/java/info/tritusk/fullwidthpunctuationfix/CommandDumpGlyphWidth.java
@@ -9,14 +9,11 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.commons.io.FileUtils;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
 
 @SideOnly(Side.CLIENT)
 @ParametersAreNonnullByDefault
@@ -40,30 +37,17 @@ public class CommandDumpGlyphWidth extends CommandBase {
             try {
                 byte[] dataToDump = FullwidthPunctuationFix.INSTANCE.getCharWidthData();
                 File dest = new File(Minecraft.getMinecraft().getResourcePackRepository().getDirResourcepacks(), "autogen_glyph_width");
-                try {
-                    Files.createDirectory(dest.toPath());
-                } catch (FileAlreadyExistsException e) {}
+                if (!dest.exists() || !dest.isDirectory()) {
+                    dest.mkdir();
+                }
                 File dumpTargetDir = new File(dest, "assets/minecraft/font");
-                try {
-                    Files.createDirectories(dumpTargetDir.toPath());
-                } catch (FileAlreadyExistsException e) {}
+                if (!dumpTargetDir.exists() || !dumpTargetDir.isDirectory()) {
+                    dumpTargetDir.mkdirs();
+                }
                 File dumpTarget = new File(dumpTargetDir, "glyph_sizes.bin");
-                try {
-                    Files.createFile(dumpTarget.toPath());
-                } catch (FileAlreadyExistsException e) {
-                    dumpTarget.delete();
-                    dumpTarget.createNewFile();
-                }
-                FileOutputStream outputStream = new FileOutputStream(dumpTarget);
-                outputStream.write(dataToDump);
-                outputStream.close();
+                FileUtils.writeByteArrayToFile(dumpTarget, dataToDump, false);
                 File packMeta = new File(dest, "pack.mcmeta");
-                if (!packMeta.exists()) {
-                    PrintWriter writer = new PrintWriter(packMeta);
-                    writer.println("{ \"pack\": { \"pack_format\": 2, \"description\": \"Auto-generated glyph width data\" } }");
-                    writer.flush();
-                    writer.close();
-                }
+                FileUtils.writeStringToFile(packMeta, "{ \"pack\": { \"pack_format\": 2, \"description\": \"Auto-generated glyph width data\" } }", "UTF-8", false);
                 sender.addChatMessage(new TextComponentTranslation("command.fwpf.dump.success"));
             } catch (Exception e) {
                 sender.addChatMessage(new TextComponentTranslation("command.fwpf.dump.fail"));
